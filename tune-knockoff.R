1a1d2a8851dba40849eadd426d6bd7bb7c333e31
Brook Luers <brook@0587381272.wireless.umich.net>
2018-12-14 09:23:16 -0500

    move vif functions into tune-knockoff

--- tune-knockoff.R
+++ tune-knockoff.R
@@ -1,27 +1,33 @@
 
-get_detfun <- function(Sigma){
+get_viffun <- function(Sigma_inv, tol=1e-5){
   f <- function(svec){
-    return(
-      prod(svec) * det(2 * Sigma - diag(svec))
-    )
+    Smat <- diag(svec)
+    V <- 2 * Smat - Smat %*% Sigma_inv %*% Smat
+    return(ifelse(min(eigen(V)$values) < tol, Inf, sum(diag(solve(V)))))
+    #return(sum(diag(solve(V))))
   }
   return(f)
 }
 
-get_detgrad <- function(Sigma){
-  p <- ncol(Sigma)
+get_vifgrad <- function(Sigma_inv){
+  p <- ncol(Sigma_inv)
+  Jlist <- vector('list', p)
+  for (k in seq_along(Jlist)){
+    Jlist[[k]] <- matrix(0,nrow=p,ncol=p)
+    Jlist[[k]][k,k] <- 1
+  }
   f <- function(svec){
-    S <- diag(svec)
-    W <- 2 * Sigma - S
-    Wdet <- det(W)
-    ret <- vector('numeric', p)
-    for (j in 1:p){
-      Jjj <- matrix(0,nrow=p,ncol=p)
-      Jjj[j,j] <- 1
-      detWgrad <- Wdet * sum(diag(solve(W, -Jjj)))
-      ret[j] <- prod(svec) * detWgrad + Wdet * prod(svec[-j])
-    }
-    return(ret) 
+    Smat <- diag(svec)
+    U <- 2 * Smat - Smat %*% Sigma_inv %*% Smat
+    Ui <- solve(U)
+    ret <- sapply(1:p, function(j){
+      dUds <- matrix(0, nrow=p, ncol=p)
+      dUds[,j] <- - ( Sigma_inv[,j] * svec )
+      dUds[j,] <- dUds[j,] - svec * Sigma_inv[j,]
+      dUds[j,j] <- dUds[j,j] + 2
+      return(sum(diag( -Ui %*% dUds %*% Ui )))
+    })
+    return(ret)
   }
   return(f)
 }
