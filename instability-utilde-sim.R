893550d9770a9bf120afa60bbaac18cb02a53680
Brook Luers <brook@dhcp-22-203.private.isr.umich.edu>
2019-09-04 14:38:10 -0400

    compute fpr

--- instability-utilde-sim.R
+++ instability-utilde-sim.R
@@ -13,6 +13,7 @@ myseed <- as.numeric(args[4])
 mycores <- as.numeric(args[5])
 sigmatype <- args[6]
 k <- as.numeric(args[7])
+offset <- as.numeric(args[8])
 if (is.na(args[9])){
   rho <- 0.8
 } else {
@@ -51,11 +52,13 @@ get_1simfun <- function(N, SigmaGen, BETA, FDR, offset = 1, stat=stat.lasso_coef
   fdp <- function(selected) sum(BETA[selected] == 0) / max(1, length(selected))
   ppv <- function(selected) sum(BETA[selected] != 0) / max(1, length(selected))
   tpr <- function(selected) sum(BETA[selected] != 0) / k
+  fpr <- function(selected) sum(BETA[selected] == 0) / (p - k)
   ret <- function(i){
     sel_all <- lapply(s_all, function(svec) return(knockoff_select(X, svec, xqr, Y, FDR, offset=offset, 
                                                                    stat = stat, 
                                                                    random = random)))
     return(sapply(sel_all, function(ss) return(c(fdp = fdp(ss),
+                                                 fpr = fpr(ss),
                                                  ppv = ppv(ss),
                                                  tpr = tpr(ss),
                                                  nsel = length(ss)))))
@@ -77,22 +80,24 @@ BETA <- vector('numeric', p)
 magnitude <- 3.5
 BETA[sample(1:p, size = k)] <- magnitude * sample(c(1,-1), size=k, replace=T)
 FDR <- 0.1
-cat("Nominal FDR = "); cat(FDR)
-sim1fun <- get_1simfun(N, SigmaGen, BETA, FDR, offset=0, stat=stat.lasso_coefdiff, random=TRUE)
+cat("Nominal FDR = "); cat(FDR); cat("\n")
+sim1fun <- get_1simfun(N, SigmaGen, BETA, FDR, offset=offset, stat=stat.lasso_coefdiff, random=TRUE)
 simres <- mclapply(1:nsim, sim1fun)
 
 res_fmt <- 
   bind_rows(lapply(simres, as_tibble, rownames='meas'),
           .id = 'sim') %>%
-  gather(sdp,ldet,equi, key='stype',value='val')%>%
+  gather(sdp, ldet, equi, key='stype', value='val')%>%
   spread(meas, val)
 res_fmt$pop_Sigma_cnum <- kappa(SigmaGen, exact = TRUE)
 res_fmt$k <- k
 res_fmt$FDR <- FDR
 res_fmt$signal <- magnitude
 res_fmt$sigmatype <- sigmatype
+res_fmt$rho <- rho
+res_fmt$offset <- offset
 
-write.table(res_fmt, file = paste("utilde-unstable-N", N, "-p", p, 
+write.table(res_fmt, file = paste("utilde-unstable-N", N, "-p", p, "-rho", rho,
                                   '.csv', 
                                   sep=''),
             quote=F, sep=',', row.names=FALSE)
